52bb3ef81223e8e82da8df0eb814b083c9dbcd6e
prof-mbsi <marlonbsi@protonmail.com>
2025-02-27 15:18:57 -0300

    Adding comments to bubble-sort.c and quick-sort.c

--- ordenacao-busca/bubble-sort.c
+++ ordenacao-busca/bubble-sort.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+//define o tamanho do vetor e facilita a criação dos loops
 #define SIZE 10
 
 int main(){
@@ -7,17 +8,22 @@ int main(){
     int i, aux, flag;
     int vetor[SIZE];
 
+    //preenche o vetor
     for(i=0; i<SIZE; i++){
         printf("Item %d: ", i + 1);
         scanf("%d", &vetor[i]);
     }
 
+    //define flag verdadeiro para executar o while pelo menos 1 vez
     flag = 1;
 
     while(flag){
+        //define o flag falso para caso não haja um swap até o final do loop
         flag = 0;
 
+        //itera analisando a necessidade de fazer o swap
         for(i=0; i<=SIZE-2; i++){
+            //caso anterior seja maior que posterior, swap e seta a flag para verdadeiro
             if(vetor[i] > vetor[i+1]){
                 aux = vetor[i];
                 vetor[i] = vetor[i+1];
@@ -28,8 +34,11 @@ int main(){
 
     }
 
+    //mostra o vetor ordenado
     for(i=0; i<SIZE; i++){
         printf("\nElemento %d : %d", i+1, vetor[i]);
     }
+
+    return 0;
 
 }
--- ordenacao-busca/quick-sort.c
+++ ordenacao-busca/quick-sort.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
 
+//função que particiona o vetor
 int particiona(int *v, int inicio, int fim){
-    int aux, pivo = (v[inicio] + v[fim] + v[(inicio + fim)/2]) /3;
+
+    //encontra um pivot. Nesse caso, busca um valor mediano
+    int aux, pivot = (v[inicio] + v[fim] + v[(inicio + fim)/2]) /3;
+
+    //checa se inicio < fim para continuar executando a função
     while(inicio < fim){
-        while(inicio < fim && v[inicio] <= pivo)
+        //move o índice em busca de um valor à esquerda maior que o pivot
+        while(inicio < fim && v[inicio] <= pivot)
             inicio++;
-        while(inicio < fim && v[fim] > pivo)
+        //move o índice em busca de um valor à direita menor que o pivot
+        while(inicio < fim && v[fim] > pivot)
             fim--;
+        //faz o swap
         aux = v[inicio];
         v[inicio] = v[fim];
         v[fim] = aux;
@@ -15,26 +22,39 @@ int particiona(int *v, int inicio, int fim){
     return inicio;
 }
 
+//função que implementa a lógica da solução
 void quickSort(int *v, int inicio, int fim){
     int pos;
+    //checa se a posição inicial está antes da posição final para continuar executando o código
     if(inicio < fim){
+        //chama o método particiona passando o vetor, posição inicial e posição final
         pos = particiona(v, inicio, fim);
+
+        //chama quickSort para o vetor dividido à esquerda
         quickSort(v, inicio, pos-1);
+
+        //chama quickSort para o vetor dividido à direita
         quickSort(v, pos, fim);
     }
 }
 
+//função main que executa o programa
 int main(){
 
+    //cria estaticamente o vetor de 15 posições
     int vetor[] = {47, 35, 12, 49, 56, 25, 71, 83, 5, 3, 1, 70, 99, 10, 61};
 
+    //mostra o vetor na ordem orignial
     for(int i=0; i<15; i++){
         printf("\n%d", vetor[i]);
     }
 
     printf("\n");
+
+    //chama a fução quickSort pela primeira vez passando o vetor, posição inicial e posição final
     quickSort(vetor, 0, 14);
 
+    //mostra o vetor ordenado
     for(int i=0; i<15; i++){
         printf("\n%d", vetor[i]);
     }
